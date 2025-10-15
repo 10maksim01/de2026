71fb786a18b55a72f1a95e32466499854ff33bc0
10maksim01 <66488898+10maksim01@users.noreply.github.com>
2025-10-15 12:41:34 +0300

    Update PVE-DE-BASH.sh

--- PVE-DE-BASH.sh
+++ PVE-DE-BASH.sh
@@ -1334,14 +1334,14 @@ function deploy_stand_config() {
 
         set_netif_conf test && set_disk_conf test || exit 1
 
-        for opt in $( printf '%s\n' "${!vm_config[@]}" | sort -V ); do
+          for opt in $(printf '%s\n' "${!vm_config[@]}" | sort); do
             case "$opt" in
                 startup|tags|ostype|serial[0-3]|agent|scsihw|cpu|cores|memory|bwlimit|description|args|arch|vga|kvm|rng0|acpi|tablet|reboot|startdate|tdf|cpulimit|cpuunits|balloon|hotplug)
                     cmd_line+=" --$opt '${vm_config[$opt]}'";;
                 network*) set_netif_conf "$opt" "${vm_config[$opt]}";;
                 bios) [[ "${vm_config[$opt]}" == ovmf ]] && cmd_line+=" --bios 'ovmf' --efidisk0 '${config_base[storage]}:0,format=$config_disk_format'" || cmd_line+=" --$opt '${vm_config[$opt]}'";;
                 ?(boot_)@(disk|iso)_+([0-9])) set_disk_conf "$opt" "${vm_config[$opt]}";;
-                access_role) ${config_base[access_create]} && set_role_config "${vm_config[$opt]}";;
+                access_roles) ${config_base[access_create]} && set_role_config "${vm_config[$opt]}";;
                 machine) set_machine_type "${vm_config[$opt]}";;
                 *) echo_warn "[Предупреждение]: обнаружен неизвестный параметр конфигурации '$opt = ${vm_config[$opt]}' ВМ '$elem'. Пропущен"
             esac
