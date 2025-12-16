dbc252ab233e75fdfb5bfdcd5554ddb30c9a7e8a
10maksim01 <66488898+10maksim01@users.noreply.github.com>
2025-12-16 11:28:12 +0300

    Update PVE-DE-BASH1.sh

--- PVE-DE-BASH1.sh
+++ PVE-DE-BASH1.sh
@@ -590,9 +590,9 @@ function get_file() {
     else
         filename=$url
 	}
-#    fi
-#    [[ -r "$filename" ]] || { echo_err "Ошибка: файл '$filename' должен существовать и быть доступен для чтения"; exit 1; }
-#    list_url_files["$md5"]="$url"
+    fi
+    [[ -r "$filename" ]] || { echo_err "Ошибка: файл '$filename' должен существовать и быть доступен для чтения"; exit 1; }
+    list_url_files["$md5"]="$url"
 }
 
 function set_configfile() {
