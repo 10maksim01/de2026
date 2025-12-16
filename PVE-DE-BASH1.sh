2690505235ee670e826543a6a26517251f16453f
10maksim01 <66488898+10maksim01@users.noreply.github.com>
2025-12-16 11:13:57 +0300

    Update PVE-DE-BASH1.sh

--- PVE-DE-BASH1.sh
+++ PVE-DE-BASH1.sh
@@ -589,10 +589,11 @@ function get_file() {
         url="$filename"
     else
         filename=$url
+	}
 #    fi
 #    [[ -r "$filename" ]] || { echo_err "Ошибка: файл '$filename' должен существовать и быть доступен для чтения"; exit 1; }
 #    list_url_files["$md5"]="$url"
-#}
+}
 
 function set_configfile() {
 
